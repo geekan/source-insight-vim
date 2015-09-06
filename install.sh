c0c180ae179ba27c5345aa50d0b4b3d80b0f8d29
geekan <geekan@foxmail.com>
2015-09-06 20:46:49 +0800

    use timestamp to bakup vimrc.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv ~/.vimrc ~/.vimrc.bakup
+mv ~/.vimrc ~/.vimrc.bakup.`date "+%Y%m%d.%T"`
 cp .*vimrc ~/
 vim +PluginInstall +qall
